c372c2a12596fb66facd4a6a38110134731cf0ff
Dan Allison <daniel.j.allison@gmail.com>
2017-12-15 14:29:54 -0800

    Replace "quux" with "ready"

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-meta_app = App.new(name: 'QuuxBase', public_id: Rails.application.config.meta_app_id, owner_id: 0)
+meta_app = App.new(name: 'ReadyBase', public_id: Rails.application.config.meta_app_id, owner_id: 0)
 meta_app.apply_defaults # calling this manually since we are skipping validations
 meta_app.save(validate: false) # owner_id: 0 is not valid, so skipping validation
 meta_user = User.create(app_id: meta_app.id, username: 'meta_user', email: 'meta@readybase.org', password: 'password')
